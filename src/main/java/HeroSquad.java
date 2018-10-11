6f9d1c4a542952ac8a47ecc7f6dd1862475c8659
ArnoldHenry <arnoldomwoyo2@gmail.com>
2018-10-11 11:31:12 +0300

    added squadform

--- src/main/java/HeroSquad.java
+++ src/main/java/HeroSquad.java
@@ -64,6 +64,12 @@ public static void main(String[]args){
         return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/squadform",(request, response) ->{
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 }
 
 }
